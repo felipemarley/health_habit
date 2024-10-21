180ed8c08e5b5492fe6a9992d96a6830b68c58d6
Henry Medeiros <henrymedeiros77@gmail.com>
2024-10-20 22:33:04 -0300

    add first screen

---
+++ lib/widgets/HabitsList.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:health_habit/models/Habit.dart';
+import 'package:health_habit/constants/CategoryConstants.dart';
+
+class HabitsList extends StatelessWidget {
+  final List<Habit> _habits = [
+    Habit(
+      name: "Ir à academia",
+      description: "Ir à academia 3 vezes por semana",
+      category: CategoryConstants.sports,  // Categoria de esportes
+    ),
+    Habit(
+      name: "Aprender guitarra",
+      description: "Praticar guitarra 1 hora por dia",
+      category: CategoryConstants.music,  // Categoria de música
+    ),
+    Habit(
+      name: "Ler um livro",
+      description: "Ler 20 páginas por dia",
+      category: CategoryConstants.tech,  // Categoria de tecnologia
+    ),
+    // Adicione mais hábitos conforme necessário
+  ];
+
+  HabitsList({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      itemCount: _habits.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          leading: _habits[index].category.icon,
+          title: Text(_habits[index].name),
+          trailing: Icon(Icons.check_box_outline_blank),
+        );
+      },
+      separatorBuilder: (context, index) => Divider(color: Colors.black,),
+    );
+  }
+}
