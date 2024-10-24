564ed3ce60b5c3c8530ecf0960f5e819249d8680
Henry Medeiros <henrymedeiros77@gmail.com>
2024-10-23 23:00:38 -0300

    changing models

--- lib/models/Activity.dart
+++ lib/models/Activity.dart
@@ -3,10 +3,12 @@ import 'package:health_habit/models/enums/ActivityStatus.dart';
 import 'package:uuid/uuid.dart';
 
 class Activity {
+  static final Uuid _uuid = Uuid();  // Static instance of Uuid
+
   final String activityId;
   final String name;
   final String? description;
-  ActivityStatus status;
+  ActivityStatus status;  // Consider if this should be mutable or not
   final Category category;
 
   Activity({
@@ -14,5 +16,5 @@ class Activity {
     this.description,
     required this.category,
     this.status = ActivityStatus.pending,
-  }) : activityId = const Uuid().v4();
+  }) : activityId = _uuid.v4();  // Use the static Uuid instance
 }
