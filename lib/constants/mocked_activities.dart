0c26f1bf13b6abcd0b9f07ed867350d166e360ea
Henry Medeiros <henrymedeiros77@gmail.com>
2024-10-23 23:22:31 -0300

    add more mocked activities

--- lib/constants/mocked_activities.dart
+++ lib/constants/mocked_activities.dart
@@ -26,6 +26,47 @@ final List<Activity> activities = [
       description: "Comprar presentes de aniversário",
       category: CategoryConstants.task,
     ),
-  ];
+    Habit(
+      name: "Prática de Yoga",
+      description: "Fazer yoga todas as manhãs para melhorar a flexibilidade e reduzir o estresse",
+      category: CategoryConstants.task,
+    ),
+    Task(
+      name: "Organizar a despensa",
+      description: "Verificar validades e organizar os alimentos na despensa",
+      category: CategoryConstants.task,
+    ),
+    Habit(
+      name: "Estudo de idiomas",
+      description: "Estudar francês por 30 minutos diariamente usando um aplicativo",
+      category: CategoryConstants.task,
+    ),
+    Task(
+      name: "Agendar reuniões semanais",
+      description: "Agendar reuniões semanais com a equipe para sincronizar projetos",
+      category: CategoryConstants.task,
+    ),
+    Habit(
+      name: "Hidratação",
+      description: "Beber ao menos 2 litros de água todos os dias",
+      category: CategoryConstants.task,
+    ),
+    Task(
+      name: "Renovar seguro do carro",
+      description: "Comparar preços e renovar o seguro do carro antes do vencimento",
+      category: CategoryConstants.task,
+    ),
+    Habit(
+      name: "Jardinagem",
+      description: "Cuidar do jardim todas as tardes, regando e podando as plantas",
+      category: CategoryConstants.task,
+    ),
+    Task(
+      name: "Preparar a apresentação do projeto",
+      description: "Preparar slides para a apresentação do novo projeto no próximo encontro com investidores",
+      category: CategoryConstants.task,
+    ),
+];
+
 
 
