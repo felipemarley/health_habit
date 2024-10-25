96df63a2506477d637957433258c9e4f17d3c66f
felipemarley <111880016+felipemarley@users.noreply.github.com>
2024-10-24 21:34:18 -0300

    CategoryScreen first prototype

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:health_habit/screens/MainScreen.dart';
+import 'package:health_habit/screens/PickCategoryScreen.dart';
 import 'package:health_habit/widgets/AppDrawer.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(appBar: AppBar(title: const Text("Habits")), drawer: const Drawer(child: AppDrawer()) , body: const MainScreen()),
+      home: Scaffold(appBar: AppBar(title: const Text("Habits")), drawer: const Drawer(child: AppDrawer()) , body: const PickCategoryScreen()),
     );
   }
 }
--- lib/screens/PickCategoryScreen.dart
+++ lib/screens/PickCategoryScreen.dart
@@ -1,10 +1,64 @@
 import 'package:flutter/material.dart';
-
+import 'package:health_habit/models/Category.dart';
+import 'package:health_habit/constants/CategoryConstants.dart'; 
 class PickCategoryScreen extends StatelessWidget {
   const PickCategoryScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final List<Category> categories = [
+      CategoryConstants.sports,
+      CategoryConstants.esports,
+      CategoryConstants.gym,
+      CategoryConstants.music,
+      CategoryConstants.tech,
+      CategoryConstants.alarm,
+      CategoryConstants.chill,
+      CategoryConstants.sleep,
+      CategoryConstants.bus_time,
+      CategoryConstants.birthday,
+      CategoryConstants.task,
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Escolha uma Categoria para sua Atividade'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, 
+            crossAxisSpacing: 16, // Espaçamento entre colunas
+            mainAxisSpacing: 16, // Espaçamento entre linhas
+            childAspectRatio: 3, // Ajusta a proporção para exibir o ícone e nome
+          ),
+          itemCount: categories.length,
+          itemBuilder: (context, index) {
+            final category = categories[index];
+            return GestureDetector(
+              onTap: () {
+                //print('Categoria selecionada: ${category.name}');
+              },
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      category.icon, 
+                      const SizedBox(width: 8), // Espaçamento entre o ícone e o nome
+                      Text(category.name), 
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
